93899165ca63644d572dda504a683157db808c1d
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2021-11-28 16:59:53 -0600

    second commit

---
+++ src/TodoCounter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TodoCounter(){
+  return (
+    <h2>Has compleatado 2 de 3 tareas pendientes</h2>
+  );
+}
+
+export { TodoCounter };
---
+++ src/TodoSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoSearch() {
+  return (
+    <div>
+      <input placeholder="Carro"/>
+    </div>
+  )
+}
+
+export {TodoSearch};
